9bb125468da2fd54407f64543f345afaff3bd6b5
z987645344-arch <z987645344@gmail.com>
2026-09-11 18:51:11 +0800

    build: 更新APK版本至0.5.0
    - versionCode 由 5 更新为 6
    - versionName 由 0.4.0 更新为 0.5.0
    - prod/qa 两个 flavor 单元测试通过，并重新构建 Debug APK 后以 aapt 核验包内版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.zhiliaohub.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.4.0"
+        versionCode = 6
+        versionName = "0.5.0"
 
     }
 
